a89e8f64f3d5432cde3a77fe1498542f840e4f0c
Maarten van der Heijden <maarten.van.der.heijden@ing.com>
2022-10-17 14:13:47 +0200

    Add readme

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
-# Gorm Deep Filtering Plugin
+# 🌌 Gorm Deep Filtering Plugin
 
-Work in Progress
+Ever wanted to filter objects on a deep level using only maps? This plugin allows you to do just that.
+
+```go
+package main
+
+func main () {
+	filters := map[string]any{
+		"name": "abc",
+		"related_object": map[string]any{
+			"title": "engineer",
+		},
+	}
+}
+```
+
+Is automatically turned into a query that looks like this:
+
+```sql
+SELECT * FROM employees WHERE related_object_id IN (SELECT id FROM occupations WHERE title = "engineer")
+```
+
+## ⬇️ Installation
+
+`go get github.com/survivorbat/gorm-deep-filtering`
+
+## 📋 Usage
+
+Check out [the examples](./examples_test.go).
+
+## 🔭 Plans
+
+Better error handling, logging.
---
+++ examples_test.go
@@ -0,0 +1,47 @@
+package deepgorm
+
+import (
+	"fmt"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func ExampleNew() {
+	db, _ := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+
+	_ = db.Use(New())
+}
+
+// Get all ObjectAs that are connected to ObhectB with Id 50
+func ExampleAddDeepFilters() {
+	type ObjectB struct {
+		ID int
+	}
+
+	type ObjectA struct {
+		ID int
+
+		ObjectB   *ObjectB `gorm:"foreignKey:ObjectBID"`
+		ObjectBID int
+	}
+
+	db, _ := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	_ = db.Use(New())
+
+	db.Create(&ObjectA{
+		ID: 1,
+		ObjectB: &ObjectB{
+			ID: 50,
+		},
+	})
+
+	filters := map[string]any{
+		"object_b": map[string]any{
+			"id": 50,
+		},
+	}
+
+	var result ObjectA
+	db.Where(filters).Find(&result)
+	fmt.Println(result)
+}
